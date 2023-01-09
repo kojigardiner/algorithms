3850a2e62c4bab7d6bc511b0bbe1b32094cfa271
Koji Gardiner <koji.gardiner@gmail.com>
2023-01-09 14:02:31 -0800

    Remove redundant string references from CircularSuffix

--- java/10_burrows/CircularSuffixArray.java
+++ java/10_burrows/CircularSuffixArray.java
@@ -8,31 +8,26 @@ import edu.princeton.cs.algs4.IndexMinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
 public class CircularSuffixArray {
+    private final String str;
     private final int len;                // string length
     private final int[] sortedIdx;        // sorted suffix indices
 
     // Private nested class that enables memory-efficient construction of the
-    // circular suffix array. This class stores the string length, a reference
-    // to the string object, and the index where the suffix starts. It
-    // implements the Comparable interface by performing a
+    // circular suffix array. This class stores only the index where the suffix
+    // starts. It implements the Comparable interface by performing a
     // character-by-character comparison between two CircularSuffix objects
     // with different starting indices.
     private class CircularSuffix implements Comparable<CircularSuffix> {
-        private final String s;
-        private final int len;
         private final int idx;
 
-        public CircularSuffix(String str, int i) {
-            s = str;
+        public CircularSuffix(int i) {
             idx = i;
-            len = s.length();
         }
 
         public int compareTo(CircularSuffix cs) {
-            int minLen = Math.min(len, cs.len);
-            for (int i = 0; i < minLen; i++) {
-                char c1 = s.charAt((idx + i) % len);
-                char c2 = cs.s.charAt((cs.idx + i) % cs.len);
+            for (int i = 0; i < len; i++) {
+                char c1 = str.charAt((idx + i) % len);
+                char c2 = str.charAt((cs.idx + i) % len);
                 if (c1 < c2) {
                     return -1;
                 }
@@ -51,6 +46,7 @@ public class CircularSuffixArray {
         }
 
         // Cache the length for O(1) lookup
+        str = s;
         len = s.length();
 
         // Create a priority queue to implicitly sort the suffix strings
@@ -59,7 +55,7 @@ public class CircularSuffixArray {
         // Insert into the priority queue each shifted version of the string
         // with a new index
         for (int i = 0; i < len; i++) {
-            pq.insert(i, new CircularSuffix(s, i));
+            pq.insert(i, new CircularSuffix(i));
         }
 
         // Initial the sorted index array
